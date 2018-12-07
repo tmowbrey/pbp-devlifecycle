a619be44c06934228510ab1e3c7b2cabbdd8a0a5
Tyler Mowbrey <tmowbrey@gmail.com>
2018-12-07 09:26:28 -0800

    SFDX Source

---
+++ helper-script.sh
@@ -0,0 +1,16 @@
+while getopts m:b:d:n: option
+do
+case "${option}"
+in
+m) MESSAGE=${OPTARG};;
+b) BRANCH=${OPTARG};;
+d) DEFAULTORG=${OPTARG};;
+n) SCRATCHORGNAME=${OPTARG};;
+esac
+done
+git add .
+git commit -m "${MESSAGE}"
+git push -u origin ${BRANCH}
+sfdx force:org:create -v ${DEFAULTORG} -f /config/project-scratch-def.json -a ${SCRATCHORGNAME} --wait 3
+sfdx force:org:display -u ${SCRATCHORGNAME}
+sfdx force:source:push -u ${SCRATCHORGNAME}
